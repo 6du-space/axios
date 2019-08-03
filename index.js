6e3c17f54ca263ce5a6d3110c7c8c907db3da82f
Tq-Lj <6du@protonmail.com>
2019-08-04 00:09:27 +0800

    .

---
+++ index.js
@@ -0,0 +1,22 @@
+(function(){
+  var axios, config, web;
+  axios = require('axios');
+  config = require("config-6du/6du.js");
+  (async function(){
+    var option, proxy, httpsProxyAgent, httpProxyAgent, web;
+    option = {
+      timeout: 6000
+    };
+    proxy = (await config.line('proxy'));
+    console.log(proxy);
+    if (proxy) {
+      httpsProxyAgent = require('https-proxy-agent');
+      httpProxyAgent = require('http-proxy-agent');
+      option.httpsAgent = new httpsProxyAgent(proxy);
+      option.httpAgent = new httpProxyAgent(proxy);
+    }
+    web = axios.create(option);
+    web.defaults.headers.common["User-Agent"] = "6du";
+  })();
+  module.exports = web;
+}).call(this);
---
+++ index.ls
@@ -0,0 +1,25 @@
+require! <[
+  axios
+]>
+config = require("config-6du/6du.js")
+
+var web
+
+do !~>
+  option = {
+      timeout: 6000,
+  }
+  proxy = await config.line \proxy
+  console.log proxy
+  if proxy
+    const httpsProxyAgent = require('https-proxy-agent')
+    const httpProxyAgent = require('http-proxy-agent')
+    option <<<
+      httpsAgent: new httpsProxyAgent(proxy),
+      httpAgent: new httpProxyAgent(proxy)
+
+  web = axios.create(option)
+  web.defaults.headers.common["User-Agent"]="6du"
+
+module.exports = web
+
